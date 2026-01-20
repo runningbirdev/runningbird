84ae55d4bd014edaabc498025c6f2ac1dde8d82a
runningbirdev <luchasjovan@gmail.com>
2026-01-20 16:24:36 +0700

    Create register.js

---
+++ register.js
@@ -0,0 +1,29 @@
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+
+const TOKEN = process.env.TOKEN;
+const CLIENT_ID = "1293170838454931467";
+const GUILD_ID = "1293545859920957490";
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("roles")
+    .setDescription("Pilih role menggunakan tombol")
+    .toJSON()
+];
+
+const rest = new REST({ version: "10" }).setToken(TOKEN);
+
+(async () => {
+  try {
+    console.log("Registering /roles command...");
+
+    await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands }
+    );
+
+    console.log("✅ /roles berhasil didaftarkan");
+  } catch (err) {
+    console.error(err);
+  }
+})();
